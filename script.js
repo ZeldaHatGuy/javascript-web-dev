78e91a73992cffd74b19a07756ee6e5b735281da
gksingl <greg.k.singletary@gmail.com>
2021-06-22 16:34:57 -0500

    adding notes from javascript course.

---
+++ script.js
@@ -0,0 +1,166 @@
+/*
+let js = 'amazing';
+if (js === 'amazing') alert('Javascript is fun!');
+
+let me = {
+    name: 'Greg',
+    Age: 29,
+}
+
+console.log(me.name)
+
+let country = 'US'
+let continent = 'North America'
+let populatoin = 100
+
+console.log(country, continent, populatoin)
+
+
+let height;
+if (height) {
+    console.log('Height is defined')
+} else {
+    console.log('Height is not defined')
+}
+const me = {
+    firstName: 'Greg',
+    birthYear: 1982,
+    heightInFeet: 6,
+}
+
+me.firstName = 'Chris'
+console.log(me.firstName)
+let yourAge = 62
+let myAge = 38
+console.log(yourAge + myAge)
+
+
+const age = 18;
+
+if (age === 18) console.log('You just became an adult')
+if (age == 18) console.log('You just became an adult')
+
+const favorite = Number(prompt("Whats your favorite number?"))
+console.log(favorite)
+console.log(typeof (favorite))
+
+if (favorite === 23) {
+    console.log(`Cool ${favorite} is an amazing number`)
+} else if (favorite === 7) {
+    console.log("7 is a cool number")
+} else {
+    console.log("Number is not 23 or 7")
+}
+
+if (favorite !== 23) console.log("This is not 23")
+*/
+
+
+// const hasDriversLicense = true
+// const hasGoodVision = true
+// const isTired = false
+
+// console.log(hasGoodVision && hasDriversLicense)
+// console.log(hasGoodVision || hasDriversLicense)
+// console.log(!hasGoodVision)
+// console.log(hasGoodVision || hasDriversLicense || isTired)
+
+// if (hasDriversLicense && hasGoodVision && !isTired) {
+//     console.log("Sarah can drive")
+
+// } else {
+//     console.log("someone else should drive")
+// }
+
+
+// let dolphinScores = [96, 108, 89]
+// let koalaScores = [8, 9, 1]
+// let dolphinAverage = (dolphinScores[0] + dolphinScores[1] + dolphinScores[2]) / 3
+// let koalaAverage = (koalaScores[0] + koalaScores[1] + koalaScores[2]) / 3
+
+// console.log(dolphinAverage)
+// console.log(koalaAverage)
+
+// if (dolphinAverage > koalaAverage && dolphinAverage >= 100) {
+//     console.log("Dolphins Win!")
+
+// } else if (koalaAverage > dolphinAverage && koalaAverage >= 100) {
+//     console.log("Koala's Win!!")
+
+// } else if (koalaAverage === dolphinAverage && koalaAverage >= 100) {
+//     console.log("its a tie!")
+// } else {
+//     console.log("No winner today!")
+// }
+
+
+// const day = 'friday';
+
+// switch (day) {
+//     case 'monday':
+//         console.log("Plan course");
+//         console.log("Some more code for Monday");
+//         break;
+//     case 'tuesday':
+//         console.log("prep theory video");
+//         break;
+//     case 'wednesday':
+//     case 'thursday':
+//         console.log("write code samples");
+//         break;
+//     case 'friday':
+//         console.log("record videos");
+//         break;
+//     case 'saturday':
+//     case 'sunday':
+//         console.log("enjoy the weekend");
+//         break;
+//     default:
+//         console.log("Not a valid day");
+
+
+
+// }
+// 
+// if (day === 'monday') {
+//     console.log("plan course");
+//     console.log("more monday code");
+// } else if (day === 'tuesday') {
+//     console.log("prep theory video");
+// } else if (day === 'wednesday' || day === 'thursday') {
+//     console.log("write code samples");
+// } else if (day === 'friday') {
+//     console.log("record videos");
+// } else if (day === 'saturday' || day === 'sunday') {
+//     console.log("enjoy the weekend");
+// } else {
+//     console.log("Invalid day");
+// }
+
+// const age = 23;
+
+// age >= 18 ? console.log("I like to drink wine") : console.log("I like to drink water");
+
+// const drink = age >= 18 ? 'wine' : 'water'
+
+// console.log(drink)
+
+// let drink2;
+// if (age >= 18) {
+//     drink2 = 'water'
+// } else {
+//     drink2 = 'wine'
+// }
+
+// console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`)
+
+
+// const bill = 275
+// const tipPercentage = bill > 50 && bill < 300 ? 0.15 : 0.20
+
+// const tip = (bill * tipPercentage)
+// console.log(`the bill was ${bill} and the tip was ${tip}. Your total is ${bill + tip}.`)
+
+
+
+
